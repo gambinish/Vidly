9ab68ed692269389017a58cb47ca6bbdfebb90ee
Nick Gambino <nick.gambino@anthologygroup.com>
2019-01-29 23:09:23 -0800

    adding initial routes

---
+++ Vidly/Controllers/CustomerController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET REQUESTS
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+        [Route("customer/all")]
+        public ActionResult CustomerList()
+        {
+            var customerList = new List<Customer>
+            {
+                new Customer() {Name = "Adam", Last = "Smith"},
+                new Customer() {Name = "Thomas", Last = "Jefferson"}
+
+            };
+            var viewModel = new CustomerViewModel
+            {
+                Customer = customerList
+            };
+            return View(viewModel);
+            //return Content("Testing Page");
+        }
+    }
+}
---
+++ Vidly/Controllers/MovieController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET REQUESTS
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+        [Route("movie/all")]
+        public ActionResult MovieList()
+        {
+            var movieList = new List<Movie>
+            {
+                new Movie() {Name = "The Fellowship of the Ring"},
+                new Movie() {Name = "The Two Towers"}
+
+            };
+            var viewModel = new MovieViewModel
+            {
+                Movie = movieList
+            };
+            return View(viewModel);
+        }
+    }
+}
